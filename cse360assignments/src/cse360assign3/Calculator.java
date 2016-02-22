c68328e64b7bcb7917ea8cae391674cc003749e9
Simon Kim <smkim6@asu.edu>
2016-02-21 23:35:06 -0700

    Version 1

---
+++ cse360assignments/src/cse360assign3/Calculator.java
@@ -0,0 +1,86 @@
+package cse360assign3;
+
+/** Calculator Program
+ * 	This class contains a constructor of a calculator
+ * 	It will be capable of performing add, subtract, multiply, and divide	
+ *  afterward user input history will be displayed
+ * 
+ * @author  :  Seong Kim
+ * @version :  Feb 21, 2016.
+ * @ID 	    :  1208036059
+ * @Lecture :  CSE360 MWF 1:30-2:20pm 
+ * @pin#    :  422
+ * 
+ */
+	
+
+public class Calculator {
+
+	private int total;
+	
+	/** Constructor
+	 * 		initializes the value of total 
+	 * 		and sets total whenever it changes
+	 */
+	public Calculator () 
+	{		
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**Return the correct number of total
+	 *  
+	 * @return	 return total
+	 * 
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**Add the given parameter (value) to the total
+	 * 
+	 * 
+	 * @param value 	number to add to the total
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**Subtract the total with the given parameter
+	 * 
+	 * 
+	 * @param value		number to subtract from the total
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**Multiply the total with the given parameter
+	 * 
+	 * @param value		number to multiply with the total
+	 */
+	public void multiply (int value)
+	{
+		
+	}
+	
+	/**Divide the total with the given parameter
+	 * 
+	 * @param value		number to divide the total with
+	 */
+	public void divide (int value)
+	{
+		
+	}
+
+	/**Return history of all actions
+	 * 
+	 * @return			string value of history
+	 */
+	public String getHistory ()
+	{
+		return "";
+	}
+} 
